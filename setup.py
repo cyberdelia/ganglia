f3eb499f2d2831a935de7f6fff88f0fcd511bd58
Timothée Peignier <timothee.peignier@tryphon.org>
2013-08-19 21:38:27 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ganglia',
-    version='0.1',
+    version='0.2',
     packages=['ganglia',],
     license='MIT',
     long_description=open('README.rst').read(),
